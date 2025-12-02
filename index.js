e840ddb697d3e824706a520f2b76091a97ad527f
johnDLC19 <0642033@hc.ac.je>
2025-12-02 16:27:40 +0000

    Creating a comment plan

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 /* Refer to the README.md for instructions on what you need to do in this project */
-
 alert("Hello, World!");
+
+// Display form to the user
+// Capture user's input on form submission
+// Validate the user's input
+//      check users name is entered7
+//      check training plan is selected
+//      check weight is provided
+//      check weight is a number
+//      check num of competitions is provided
+//      check num of competitions is a number
+//      check num of hours for private coaching is provided
+//      check num of hours for private coaching is a number between 1 and 5
+// Generate weight category based on input
+// Calculate the total cost
+// Display the total cost to the user
