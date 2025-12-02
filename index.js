6e4b1e8e0441283593e792c95444dded47de7814
johnDLC19 <0642033@hc.ac.je>
2025-12-02 18:31:53 +0000

    Captured form data
    added an id to each input field so they can be targeted by JavaScript
    Stored data in variables

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 /* Refer to the README.md for instructions on what you need to do in this project */
-alert("Hello, World!");
 
 // Display form to the user
 // Capture user's input on form submission
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function () {
+    event.preventDefault();
+  console.log("Form submitted");
+
+// Store athletes name
+let athleteName = document.querySelector("#athlete-name").value;
+// Store athletes name
+let trainingPlan = document.querySelector("#training-plan").value;
+// Store athletes name
+let currentWeight = document.querySelector("#current-weight").value;
+// Store athletes name
+let competitions = document.querySelector("#competitions").value;
+// Store athletes name
+let privateHours = document.querySelector("#private-hours").value;
+console.log({athleteName, trainingPlan, currentWeight, competitions, privateHours});
+});
+
 // Validate the user's input
 //      check users name is entered7
 //      check training plan is selected
