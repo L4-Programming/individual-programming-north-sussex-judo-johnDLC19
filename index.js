b77278b711dd4d7c891c5849154dde4571b8c60f
johnDLC19 <0642033@hc.ac.je>
2025-12-04 14:30:19 +0000

    Presence checking
    And fixed the radio buttons

--- index.js
+++ index.js
@@ -5,24 +5,46 @@
 let form = document.querySelector("form");
 
 form.addEventListener("submit", function () {
-    event.preventDefault();
+  event.preventDefault();
   console.log("Form submitted");
 
-// Store athletes name
-let athleteName = document.querySelector("#athlete-name").value;
-// Store athletes name
-let trainingPlan = document.querySelector("#training-plan").value;
-// Store athletes name
-let currentWeight = document.querySelector("#current-weight").value;
-// Store athletes name
-let competitions = document.querySelector("#competitions").value;
-// Store athletes name
-let privateHours = document.querySelector("#private-hours").value;
-console.log({athleteName, trainingPlan, currentWeight, competitions, privateHours});
+  // Store athletes name
+  let athleteName = document.querySelector("#athlete-name").value;
+  // Store athletes name
+  let trainingPlan = form.elements["training-plan"].value;
+  // Store athletes name
+  let currentWeight = document.querySelector("#current-weight").value;
+  // Store athletes name
+  let competitions = document.querySelector("#competitions").value;
+  // Store athletes name
+  let privateHours = document.querySelector("#private-hours").value;
+
+  // Validate the user's input
+  // check users name is entered
+  if (athleteName === "") {
+    alert("Please enter your name");
+    return;
+  }
+  // check training plan is selected
+  if (trainingPlan === "") {
+    alert("Please select a training plan");
+    return;
+  }
+  // check weight is provided
+  if (currentWeight === "") {
+    alert("Please enter your current weight");
+    return;
+  }
+
+  console.log({
+    athleteName,
+    trainingPlan,
+    currentWeight,
+    competitions,
+    privateHours,
+  });
 });
 
-// Validate the user's input
-//      check users name is entered7
 //      check training plan is selected
 //      check weight is provided
 //      check weight is a number
